e6c5057962585cfe2038eb0543acf8a5b3da87cd
nanahpang <nanahpang@gmail.com>
2018-03-18 12:27:57 -0700

    update toplevel.ml

--- toplevel.ml
+++ toplevel.ml
@@ -29,11 +29,11 @@ let () =
   let ast = Parser.program Scanner.token lexbuf in  
   match !action with
     Ast -> print_string (Ast.string_of_program ast)
-  (* | _ -> let sast = Semant.check ast in
+  | _ -> let sast = Semant.check ast in
     match !action with
       Ast     -> ()
     | Sast    -> print_string (Sast.string_of_sprogram sast)
-    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
+    (* | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
     | Compile -> let m = Codegen.translate sast in
 	Llvm_analysis.assert_valid_module m;
 	print_string (Llvm.string_of_llmodule m) *)
