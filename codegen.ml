eaee421b5b486c09e9ca93c3f87bfe87c71bf7b8
jiayangli2 <jl4305@columbia.edu>
2018-03-19 23:32:07 -0400

    update codegen

--- codegen.ml
+++ codegen.ml
@@ -25,13 +25,20 @@ let translate (globals, functions) =
   let context    = L.global_context () in
   (* Add types to the context so we can use them in our LLVM code *)
   let i32_t      = L.i32_type    context
+  and i64_t      = L.i64_type    context
   and i8_t       = L.i8_type     context 
   and i1_t       = L.i1_type     context
   and float_t    = L.double_type context
-  and void_t     = L.void_type   context 
+  and void_t     = L.void_type   context
+  
   (* Create an LLVM module -- this is a "container" into which we'll 
      generate actual code *)
   and the_module = L.create_module context "MicroC" in
+  let ip_t       = L.pointer_type i8_t in
+  let ipp_t      = L.pointer_type ip_t
+  and ip32_t     = L.pointer_type i32_t in
+  let struct_t   = L.struct_type context [|i32_t; i32_t; i32_t; ip_t|] in
+  let structp_t  = L.pointer_type struct_t                  in
 
   (* Convert MicroC types to LLVM types *)
   let ltype_of_typ = function
@@ -39,7 +46,8 @@ let translate (globals, functions) =
     | A.Bool  -> i1_t
     | A.Float -> float_t
     | A.Void  -> void_t
-
+    | A.Arr   -> structp_t
+    | A.String -> ip_t
     | t -> raise (Failure ("Type " ^ A.string_of_typ t ^ " not implemented yet"))
   in
 
@@ -55,6 +63,20 @@ let translate (globals, functions) =
   let printf_func : L.llvalue = 
      L.declare_function "printf" printf_t the_module in 
 
+  let loadimg_t = L.function_type structp_t [| ip_t |] in
+  let loadimg_func = L.declare_function "load" loadimg_t the_module in
+
+  let closeimg_t = L.function_type i32_t [| structp_t |] in
+  let closeimg_func = L.declare_function "close" closeimg_t the_module in
+
+  let saveimg_t = L.function_type i32_t [| structp_t; ip_t |] in
+  let saveimg_func = L.declare_function "save" saveimg_t the_module in
+
+  let length_t = L.function_type i32_t [| structp_t |] in
+  let length_func = L.declare_function "length" length_t the_module in
+
+  let addPixel_t = L.function_type i32_t [| structp_t |] in
+  let length_func = L.declare_function "addPixel" addPixel_t the_module in
 
   let to_imp str = raise (Failure ("Not yet implemented: " ^ str)) in
 
@@ -66,7 +88,7 @@ let translate (globals, functions) =
     (* Create an Instruction Builder, which points into a basic block
       and determines where the next instruction should be placed *)
     let builder = L.builder_at_end context (L.entry_block the_function) in
-    let init_list x = "%d" in
+    let init_list _ = "%d" in
     let create_fmt_str len = "[" ^ (String.concat "," (List.init len init_list)) ^ "]\n" in
     (* Create a pointer to a format string for printf *)
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder
@@ -107,8 +129,20 @@ let translate (globals, functions) =
       | SId s -> L.build_load (lookup s) s builder
       | SArrliteral s -> 
       let to_array x = let y = snd x in match y with
-       SLiteral i2 -> L.const_int i32_t i2
-        in L.const_array i32_t (Array.of_list (List.map to_array s))
+       SLiteral i2 -> L.const_int i8_t i2 (* store [3 x i8] c"\01\02\03", i8** %img *)
+       in let img_array = L.const_array i8_t (Array.of_list (List.map to_array s))
+       in let ipt = L.define_global "tmp" img_array the_module
+       in let img_array_ptr = L.build_pointercast ipt ip_t "tmp" builder
+       in let const_arr = (L.const_struct context [|L.const_int i32_t (List.length s); L.const_int i32_t 0; L.const_int i32_t 0; img_array_ptr|]  )
+       in let global_arr = L.define_global "tmp" const_arr the_module 
+       in let res = global_arr(*L.build_struct_gep global_arr 0 "tmp" builder*)
+       in res
+      | SArrsub (e, l) -> let expr_builder = expr builder e in let l2 = expr builder l in
+        let expr_builder = expr_builder in (*L.build_gep expr_builder [|L.const_int i32_t |] "tmp" builder in*)
+        let arr_builder = L.build_struct_gep expr_builder 3 "tmp" builder in
+        let abxd = L.build_load arr_builder "tmp" builder in 
+        let abxd = L.build_in_bounds_gep abxd [|l2|] "tmp" builder in
+        L.build_load abxd "tmp" builder
       | SBinop (e1, op, e2) ->
     let (t, _) = e1
     and e1' = expr builder e1
@@ -156,21 +190,35 @@ let translate (globals, functions) =
       | SCall ("printf", [e]) ->
     L.build_call printf_func [| float_format_str ; (expr builder e) |]
       "printf" builder
+      | SCall ("length", [e]) -> let expr_builder = expr builder e in 
+        let expr_builder = L.build_struct_gep expr_builder 0 "tmp" builder in
+        L.build_load expr_builder "tmp" builder
       | SCall ("printline", [e]) -> 
     L.build_call printf_func [| string_format_str ; (expr builder e) |]
       "printf" builder
-      | SCall ("printarr", [e]) -> 
-    let expr_builder = expr builder e in
-    let init_array idx = L.const_extractvalue expr_builder [|idx|] in
-    let (_, e') = e in
-    let len_of_list = match e' with
-    SArrliteral s -> List.length s in
-    let args_array = Array.init len_of_list init_array in
-    L.build_call printf_func (Array.append [| arr_format_str len_of_list |] args_array)
-      "printf" builder
+      | SCall ("addPixel", [e]) ->
+        let expr_builder = expr builder e in 
+        let arr_builder = L.build_struct_gep expr_builder 3 "tmp" builder in
+        let abxd_p = L.build_load arr_builder "tmp" builder in 
+        let abxd = L.build_in_bounds_gep abxd_p [|L.const_int i32_t (0)|] "tmp" builder in
+        let p_val = L.build_load abxd "tmp" builder in
+        let n_val = L.build_add p_val (L.const_int i8_t 20) "tmp" builder in
+        L.build_store n_val abxd builder 
+      | SCall("load", [e]) ->
+    L.build_call loadimg_func [| (expr builder e) |] "load" builder
+      | SCall("close", [e]) ->
+    L.build_call closeimg_func [| (expr builder e) |] "close" builder
+      | SCall("save", [e1; e2]) ->
+    L.build_call saveimg_func [| (expr builder e1); (expr builder e2) |] "save" builder
       | _ -> to_imp (string_of_sexpr (A.Int,e))  
     in
     (* Deal with a block of expression statements, terminated by a return *)
+    let add_terminal builder f =
+
+      match L.block_terminator (L.insertion_block builder) with
+  Some _ -> ()
+      | None -> ignore (f builder) in
+
     let rec stmt builder = function
   SBlock sl -> List.fold_left stmt builder sl
       | SExpr e -> let _ = expr builder e in builder 
@@ -178,6 +226,36 @@ let translate (globals, functions) =
                               A.Int -> L.build_ret (expr builder e) builder 
                             | _ -> to_imp (A.string_of_typ fdecl.styp)
                      in builder
+      | SIf (predicate, then_stmt, else_stmt) ->
+         let bool_val = expr builder predicate in
+   let merge_bb = L.append_block context "merge" the_function in
+         let branch_instr = L.build_br merge_bb in
+   let then_bb = L.append_block context "then" the_function in
+         let then_builder = stmt (L.builder_at_end context then_bb) then_stmt in 
+   let () = add_terminal then_builder branch_instr in
+
+   let else_bb = L.append_block context "else" the_function in
+         let else_builder = stmt (L.builder_at_end context else_bb) else_stmt in
+   let () = add_terminal else_builder branch_instr in
+
+   let _ = L.build_cond_br bool_val then_bb else_bb builder in
+
+   L.builder_at_end context merge_bb
+
+      | SWhile (predicate, body) ->
+   let pred_bb = L.append_block context "while" the_function in
+   let _ = L.build_br pred_bb builder in
+   let body_bb = L.append_block context "while_body" the_function in
+         let while_builder = stmt (L.builder_at_end context body_bb) body in
+   let () = add_terminal while_builder (L.build_br pred_bb) in
+   let pred_builder = L.builder_at_end context pred_bb in
+   let bool_val = expr pred_builder predicate in
+   let merge_bb = L.append_block context "merge" the_function in
+   let _ = L.build_cond_br bool_val body_bb merge_bb pred_builder in
+   L.builder_at_end context merge_bb
+
+      | SFor (e1, e2, e3, body) -> stmt builder
+      ( SBlock [SExpr e1 ; SWhile (e2, SBlock [body ; SExpr e3]) ] )
       | s -> to_imp (string_of_sstmt s)
     (* Generate the instructions for the function's body, 
        which mutates the_module *)
