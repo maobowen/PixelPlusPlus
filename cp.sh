2ced4adc2199d43fda419e871a63250be438ac7e
jiayangli2 <jl4305@columbia.edu>
2018-04-11 20:51:11 -0400

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -2,7 +2,7 @@
 
 ./toplevel.native test2.xpp > test2.ll
 ./toplevel.native -c2 builtin.xpp > builtin.ll
-llc test2.ll > test2.s
-llc builtin.ll > builtin.s
+llc-6.0 test2.ll > test2.s
+llc-6.0 builtin.ll > builtin.s
 gcc -c load.c
 cc -o test2.exe test2.s builtin.s load.o -lm
