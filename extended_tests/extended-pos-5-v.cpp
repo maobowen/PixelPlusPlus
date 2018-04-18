dc10a398ab9cbffc35e3e575f6bbd94741dac600
jiayangli2 <jl4305@columbia.edu>
2018-04-18 10:41:22 -0400

    Update extended-pos-5-v.cpp

--- extended_tests/extended-pos-5-v.cpp
+++ extended_tests/extended-pos-5-v.cpp
@@ -17,9 +17,19 @@ int main() {
 
     const char* path2 = "./extended-pos-5-v.png";
     uint8_t image2[length1];
-
+    uint8_t image3[length1];
     for (int i = 0; i < length1; i++) {
-        image2[length1 - 1 - i] = image1[i];
+	image3[length1 - 1 - i] = image1[i];
+    }
+	
+    for (int i = 0; i < height1; i++) {
+    	for (int j = 0; j < width1; j++) {
+	    int pixel = (i * width1 + j) * 3;
+	    int pixel2 = ((height - 1 - i) * width1 + (width1 - 1 - j)) * 3;
+	    image2[pixel] = image3[pixel2];
+	    image2[pixel + 1] = image3[pixel2 + 1];
+	    image2[pixel + 3] = image3[pixel2 + 2];
+	}
     }
     
     stbi_write_png(path2, width1, height1, CHANNEL_NUM, image2, width1 * CHANNEL_NUM);
