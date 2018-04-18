5a5b65025b95528c5d738aefb02c6b32d03e5b32
Bowen Mao <b.mao@columbia.edu>
2018-04-18 04:42:50 -0400

    Completed test script for the extended test suite.
    - Completed test script for the extended test suite;
    - Refactored test case #4 and #6.

--- test-extended.sh
+++ test-extended.sh
@@ -6,11 +6,11 @@ files="extended-*.xpp"
 TOPLEVEL="./toplevel.native"
 LLC="llc"
 CLANG="clang"
+HASH="sha256sum"
 
 CheckPass() {
     basename=`echo $1 | sed 's/.*\\///
                              s/.xpp//'`
-    reffile=`echo $1 | sed 's/.xpp$//'`
 
     echo "Testing ${basename}..."
 
@@ -23,20 +23,51 @@ CheckPass() {
     # Testing
     if [[ -f ${basename}.out ]]; then  # If the comparison file exists
         if diff -bB ${basename}.xpp.out ${basename}.out > ${basename}.diff; then
-            echo "Test passed."
+            echo "Positive test passed. The output matches."
         else
-            echo "Test failed."
+            echo "Positive test failed. The output does not match."
             diff -bB ${basename}.xpp.out ${basename}.out
         fi
+        # Clean up
         rm -f ${basename}.diff
     else
         # Build verification program
         g++ -std=c++11 -g -Wall ${basename}-v.cpp -o ${basename}-v
         ./${basename}-v
+        # Compare two images
+        hash1=`${HASH} ${basename}.png | cut -d\  -f1`
+        hash2=`${HASH} ${basename}-v.png | cut -d\  -f1`
+        if [ ${hash1} == ${hash2} ]; then
+           echo "Positive test passed. Images are identical."
+        else
+           echo "Positive test failed. Images are different."
+           ${HASH} ${basename}.png ${basename}-v.png
+        fi
+        # Clean up
+        rm -f ${basename}-v ${basename}-v.png
     fi
 
     # Clean up
-    rm -f ${basename}.ll ${basename}.s ${basename} ${basename}.xpp.out ${basename}-v
+    rm -f ${basename}.ll ${basename}.s ${basename} ${basename}.xpp.out
+}
+
+CheckFail() {
+    basename=`echo $1 | sed 's/.*\\///
+                             s/.xpp//'`
+
+    echo "Testing ${basename}..."
+
+    # Build Pixel++ program
+
+    ../${TOPLEVEL} $1 2> ${basename}.xpp.err
+    cat ${basename}.xpp.err
+
+    if diff -bB ${basename}.xpp.err ${reffile}.err > ${basename}.diff; then
+        echo "Negative test passed. The expected error messages are generated."
+    else
+        echo "Negative test failed. The expected error messages are not generated."
+    fi  
+    rm -f ${basename}.xpp.err ${basename}.diff
 }
 
 cd ${basedir}
@@ -50,6 +81,9 @@ do
     case $file in
         *extended*pos*)
             CheckPass $file
+            ;;
+        *extended*neg*)
+            CheckFail $file
             ;;
         *)
             echo "unknown file type $file"
